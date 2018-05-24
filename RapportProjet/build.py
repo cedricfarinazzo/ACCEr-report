19ad1b38734419c99dafe0af924cd47a63b55de9
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-05-25 01:49:47 +0200

    RF: add image 4

--- RapportProjet/build.py
+++ RapportProjet/build.py
@@ -3,13 +3,14 @@ print("")
 print("      Building report")
 print("")
 os.system("pdflatex rapport.tex")
+os.system("makeindex rapport.tex")
 os.system("pdflatex rapport.tex")
 os.system("pdflatex rapport.tex")
 
 print("")
 print("      Clean directory")
 print("")
-files = ["rapport.aux", "rapport.log", "rapport.out", "rapport.toc", "rapport.lof", "rapport.lot", "rapport.synctex.gz"]
+files = ["rapport.aux", "rapport.log", "rapport.out", "rapport.glo", "rapport.ilg", "rapport.ind", "rapport.ist", "rapport.toc", "rapport.lof", "rapport.lot", "rapport.synctex.gz"]
 for file in files:
 	if os.path.isfile(file):
 		os.remove(file)
