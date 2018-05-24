86b1df8dacdf0234151c55c7dab870efd7809c4d
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-05-24 17:09:18 +0200

    RF: add build script

---
+++ RapportProjet/build.py
@@ -0,0 +1,17 @@
+import os
+print("")
+print("      Building report")
+print("")
+os.system("pdflatex rapport.tex")
+os.system("pdflatex rapport.tex")
+os.system("pdflatex rapport.tex")
+os.system("pdflatex rapport.tex")
+
+
+print("")
+print("      Clean directory")
+print("")
+files = ["rapport.aux", "rapport.log", "rapport.out", "rapport.toc", "rapport.lof", "rapport.lot", "rapport.synctex.gz"]
+for file in files:
+	if os.path.isfile(file):
+		os.remove(file)
